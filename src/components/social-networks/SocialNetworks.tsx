ba6828abb33931159ca7f12c8541eaa3844b29b0
Irina Rusinova <rusinovarusya@mail.ru>
2022-10-01 15:07:53 +0300

    fix: fix social network link icons

--- src/components/social-networks/SocialNetworks.tsx
+++ src/components/social-networks/SocialNetworks.tsx
@@ -5,9 +5,9 @@ import styles from "./SocialNetworks.module.css";
 const SocialNetworks: FC = () => {
   return (
     <ul className={styles.socialNetworksList}>
-      <li className={styles.socialNetworkItem}><a href="#" className={styles.link} id="vk"></a></li>
-      <li className={styles.socialNetworkItem}><a href="#" className={styles.link} id="telegram"></a></li>
-      <li className={styles.socialNetworkItem}><a href="#" className={styles.link} id="whatsapp"></a></li>
+      <li className={styles.socialNetworkItem}><a href="" className={styles.link}></a></li>
+      <li className={styles.socialNetworkItem}><a href="" className={styles.link}></a></li>
+      <li className={styles.socialNetworkItem}><a href="" className={styles.link}></a></li>
     </ul>
   )
 }
