d4aa328e8688d6a62f033294fc7e626e27dff786
Irina Rusinova <rusinovarusya@mail.ru>
2022-10-04 14:51:31 +0300

    refactor: make small changes to display correctly

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,35 @@ import GroupOfGoods from "./components/group-of-goods/GroupOfGoods";
 import CardInCatalog from "./components/card-in-catalog/CardInCatalog";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import ShoppingCart from "./components/shopping-cart/ShoppingCart";
+import CardInShoppingCart from "./components/card-in-shopping-cart/CardInShoppingCart";
+import Total from "./components/total/Total";
 
 
 const App = () => {
   return (
     <HashRouter>
-      <div className="App">
-        <StorePage>
-          <Header />
-          <Routes>
-          <Route path="/" element={
-            <Catalog>
-              <GroupOfGoods title="Наушники">
-                <CardInCatalog index={0} img="apple-byz-s8521.png" name="Name" price={1} rating={4.9} />
-              </GroupOfGoods>
-              <GroupOfGoods title="Беспроводные наушники">
-                <CardInCatalog index={1} img="apple-byz-s8521.png" name="Name" price={1} rating={4.9} />
-              </GroupOfGoods>
-            </Catalog>
-          } />
-          <Route path="/shopping-cart" element={
-            <ShoppingCart />
-          } />
-          </Routes>
-          <Footer />
-        </StorePage>
-      </div>
+      <StorePage>
+        <Header />
+        <Routes>
+        <Route path="/" element={
+          <Catalog>
+            <GroupOfGoods title="Наушники">
+              <CardInCatalog index={0} img="apple-byz-s8521.png" name="Name" price={1} rating={4.9} />
+            </GroupOfGoods>
+            <GroupOfGoods title="Беспроводные наушники">
+              <CardInCatalog index={1} img="apple-byz-s8521.png" name="Name" price={1} rating={4.9} />
+            </GroupOfGoods>
+          </Catalog>
+        } />
+        <Route path="/shopping-cart" element={
+          <ShoppingCart>
+            <CardInShoppingCart/>
+            <Total />
+          </ShoppingCart>
+        } />
+        </Routes>
+        <Footer />
+      </StorePage>
     </HashRouter>
   );
 }
