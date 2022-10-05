a122f3b6a8662397d1eaac533f8e721e70c5dd7f
Irina Rusinova <rusinovarusya@mail.ru>
2022-10-05 09:41:07 +0300

    feat: make input number buttons work

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,10 +2,14 @@ import { FC, memo, PropsWithChildren } from "react";
 import styles from "./Button.module.css";
 
 
-interface ButtonProps {}
+interface ButtonProps {
+  onClick: any;
+}
 
-const Button: FC<PropsWithChildren<ButtonProps>> = ({children}) => {
-  return <button className={styles.button}>{children}</button>
+const Button: FC<PropsWithChildren<ButtonProps>> = ({onClick, children}) => {
+  return (
+    <button className={styles.button} onClick={onClick}>{children}</button>
+  )
 }
 
 export default memo(Button);
--- src/components/input-number/InputNumber.tsx
+++ src/components/input-number/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, PropsWithChildren } from "react";
+import { FC, memo, PropsWithChildren, useState } from "react";
 import Button from "../button/Button";
 import styles from "./InputNumber.module.css";
 import Minus from "../minus/Minus";
@@ -7,12 +7,21 @@ import Plus from "../plus/Plus";
 
 interface InputNumberProps {}
 
-const InputNumber: FC<PropsWithChildren<InputNumberProps>> = ({children}) => {
+const InputNumber: FC<PropsWithChildren<InputNumberProps>> = () => {
+
+  const [count, setCount] = useState(1);
+
     return (
       <div className={styles.container}>
-        <Button><Minus /></Button>
-        <div className={styles.number}>0</div>
-        <Button><Plus /></Button>
+        <Button onClick={() => setCount(count - 1)}>
+          <Minus />
+        </Button>
+        <div className={styles.number}>
+          {count}
+        </div>
+        <Button onClick={() => setCount(count + 1)}>
+          <Plus />
+        </Button>
       </div>
     )
   }
